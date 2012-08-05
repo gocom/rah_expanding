47c42f3f4da20866649f1007bcbdb07a3149676a
Jukka Svahn <web@rahinaa.biz>
2012-08-06 00:01:42 +0300

    Set the clone's min-width to 0.

--- rah_expanding.php
+++ rah_expanding.php
@@ -77,7 +77,7 @@ EOF;
 
 				$.fn.rah_expanding = function() {
 			
-					var copy = '<textarea tabindex="-1" style="position:absolute; top:-9999px; left:-9999px; right:auto; bottom:auto; -moz-box-sizing:content-box; -webkit-box-sizing:content-box; box-sizing:content-box; word-wrap:break-word; height:0 !important; min-height:0 !important; overflow:hidden">',
+					var copy = '<textarea tabindex="-1" style="position:absolute; top:-9999px; left:-9999px; right:auto; bottom:auto; -moz-box-sizing:content-box; -webkit-box-sizing:content-box; box-sizing:content-box; word-wrap:break-word; height:0 !important; min-height:0 !important; min-width:0 !important; overflow:hidden">',
 			
 					copyStyle = [
 						'font-family',
